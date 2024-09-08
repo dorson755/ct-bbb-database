a21d06881bba70addfcd32ed54d027c82c58db29
Dorson Williams <68614535+dorson755@users.noreply.github.com>
2024-09-08 20:21:21 +0000

    added App.js

---
+++ bbb-proxy-app/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import ApiTestPage from './ApiTestPage'; // Import the API testing page
+
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        {/* Top-right Test API button */}
+        <header>
+          <Link to="/api-test" className="test-api-button">
+            Test API
+          </Link>
+        </header>
+
+        {/* Define routes */}
+        <Switch>
+          <Route path="/api-test" component={ApiTestPage} />
+          <Route path="/" exact>
+            <h1>Welcome to the BigBlueButton API App</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
